b1f5e6724adb1dd838ca45b3766ea1f99426db84
Otavio <estudosprogotavio@gmail.com>
2025-02-26 17:50:50 -0300

    Mudanças na controller de Vehicle

--- src/main/java/com/dev/Zerphyis/estaciona/controller/VehicleController.java
+++ src/main/java/com/dev/Zerphyis/estaciona/controller/VehicleController.java
@@ -4,17 +4,17 @@ import com.dev.Zerphyis.estaciona.Dtos.DataVehicle;
 import com.dev.Zerphyis.estaciona.entity.vehicle.Vehicle;
 import com.dev.Zerphyis.estaciona.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity; 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/veiculos")
-public class ControllerVehicle {
+public class VehicleController {
     @Autowired
     VehicleService service;
-
 
     @PostMapping
     public ResponseEntity<Vehicle> registerVehicle(@RequestBody DataVehicle data) {
@@ -46,3 +46,5 @@ public class ControllerVehicle {
         return ResponseEntity.noContent().build();
     }
 }
+
+
