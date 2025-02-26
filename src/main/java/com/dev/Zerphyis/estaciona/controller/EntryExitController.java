df680815c5685b524661e3539d96f518746931d0
Otavio <estudosprogotavio@gmail.com>
2025-02-26 17:56:32 -0300

    Adicionando controller da entidade EntryExit

---
+++ src/main/java/com/dev/Zerphyis/estaciona/controller/EntryExitController.java
@@ -0,0 +1,44 @@
+package com.dev.Zerphyis.estaciona.controller;
+
+import com.dev.Zerphyis.estaciona.entity.entryExit.EntryExit;
+import com.dev.Zerphyis.estaciona.entity.vehicle.TypeVehicle;
+import com.dev.Zerphyis.estaciona.service.EntryExitService;
+import com.dev.Zerphyis.estaciona.service.ParkingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/registro")
+public class EntryExitController {
+    @Autowired
+    EntryExitService service;
+
+    @PostMapping("/entrada")
+    public ResponseEntity<EntryExit> entryVehicle(@RequestParam String personName,
+                                                  @RequestParam String vehiclePlate,
+                                                  @RequestParam TypeVehicle vehicleType,
+                                                  @RequestParam Long parkingId) {
+        try {
+            EntryExit entry = service.registerEntry(personName, vehiclePlate, vehicleType, parkingId);
+            return new ResponseEntity<>(entry, HttpStatus.CREATED);  // Resposta no formato JSON
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Resposta em caso de erro
+        }
+    }
+
+    // Registrar a saída de um veículo
+    @PostMapping("/saida/{id}")
+    public ResponseEntity<EntryExit> exitVehicle(@PathVariable Long id) {
+        try {
+            EntryExit exit = service.registerExit(id);
+            return new ResponseEntity<>(exit, HttpStatus.OK);  // Resposta no formato JSON
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Resposta em caso de erro
+        }
+    }
+
+    }
+
+
