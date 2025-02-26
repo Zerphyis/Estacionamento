22bfa77619063b04a08bf45004ed3fc227b17d94
Otavio <estudosprogotavio@gmail.com>
2025-02-26 17:54:12 -0300

    Adicionadno Dto da entidade Vehicle

--- src/main/java/com/dev/Zerphyis/estaciona/Dtos/DataVehicle.java
+++ src/main/java/com/dev/Zerphyis/estaciona/Dtos/DataVehicle.java
@@ -2,5 +2,5 @@ package com.dev.Zerphyis.estaciona.Dtos;
 
 import com.dev.Zerphyis.estaciona.entity.vehicle.TypeVehicle;
 
-public record DataVehicle(String mark, String model, String color, String plate, TypeVehicle type) {
+public record DataVehicle(String mark, String model, String color, String plate, TypeVehicle typeVehicle) {
 }
