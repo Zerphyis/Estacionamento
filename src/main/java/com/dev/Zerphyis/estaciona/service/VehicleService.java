a22b20560a871a06cee41e43b0d39bbe6613763b
Otavio <estudosprogotavio@gmail.com>
2025-02-26 17:50:14 -0300

    Mudanças na service de Vehicle

--- src/main/java/com/dev/Zerphyis/estaciona/service/VehicleService.java
+++ src/main/java/com/dev/Zerphyis/estaciona/service/VehicleService.java
@@ -18,47 +18,48 @@ public class VehicleService {
     VehicleRepository repository;
 
     @Transient
- public Vehicle registerVehicle(DataVehicle data){
-        if(data.plate().length() > 7){
-          throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A placa não pode ter mais de 7 caracteres.");
-     }
+    public Vehicle registerVehicle(DataVehicle data) {
+        if (data.plate().length() > 7) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A placa não pode ter mais de 7 caracteres.");
+        }
 
         Optional<Vehicle> existingVehicle = repository.findByPlate(data.plate());
         if (existingVehicle.isPresent()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe um veículo com esta placa.");
         }
 
-         var vehicle= new Vehicle(data);
-         return repository.save(vehicle);
+        var vehicle = new Vehicle(data);
+        return repository.save(vehicle);
     }
 
 
-   public List<Vehicle> listAll(){
-      return   repository.findAll();
-   }
+    public List<Vehicle> listAll() {
+        return repository.findAll();
+    }
 
     public Vehicle getVehicleByPlate(String plate) {
         return repository.findByPlate(plate)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Veículo não encontrado."));
     }
-@Transient
-public Vehicle updateVehicle(Long id,DataVehicle data){
-    Optional<Vehicle> updateVehicle =  repository.findById(id);
-    if (updateVehicle.isPresent()) {
-        Vehicle newVehicule = updateVehicle.get();
-        newVehicule.setMark(data.mark());
-        newVehicule.setModel(data.model());
-        newVehicule.setColor(data.color());
-        newVehicule.setPlate(data.plate());
-        newVehicule.setType(data.type());
-        return repository.save(newVehicule);
-    } else {
-        throw new RuntimeException("Veiculo não encontrado com o ID: " + id);
-       }
+
+    @Transient
+    public Vehicle updateVehicle(Long id, DataVehicle data) {
+        Optional<Vehicle> updateVehicle = repository.findById(id);
+        if (updateVehicle.isPresent()) {
+            Vehicle newVehicule = updateVehicle.get();
+            newVehicule.setMark(data.mark());
+            newVehicule.setModel(data.model());
+            newVehicule.setColor(data.color());
+            newVehicule.setPlate(data.plate());
+            newVehicule.setType(data.typeVehicle());
+            return repository.save(newVehicule);
+        } else {
+            throw new RuntimeException("Veiculo não encontrado com o ID: " + id);
+        }
     }
 
     @Transient
-    public  void delete(Long id){
+    public void delete(Long id) {
         if (repository.existsById(id)) {
             repository.deleteById(id);
         } else {
